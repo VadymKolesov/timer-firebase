544448c20edfb799ad3785f11f12359264c65a3b
Vadym <46047631+VadymKolesov@users.noreply.github.com>
2024-03-20 19:03:46 +0100

    edit manifest

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import { refs } from "./src/js/components/refs";
 import { checkLogin, logOut, onBack } from "./src/js/components/utilites";
 
-// checkLogin();
+checkLogin();
 
 refs.logOutBtn.addEventListener("click", logOut);
 refs.navName.innerHTML = JSON.parse(localStorage.getItem("log")).user;
